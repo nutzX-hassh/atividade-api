5734b22a70c767b82bcf06d5c2f8df7744c027cc
nutzX-hassh <arthur.c.teixeira@ba.estudante.senai.br>
2026-09-23 19:31:40 +0000

    atualiza o projeto

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,50 @@
 const express = require('express');
-
 const cors = require('cors');
- 
+
 const app = express();
 
 app.use(cors());
-
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
+    const produtos = [
+        {
+            id: 1,
+            nome: 'Iphone 20 pro max',
+            marca: 'Semsunga',
+            preco: 67000.00,
+            categoria: 'Eletrônicos',
+            Estoque: true
+        },
+        {
+            id: 2,
+            nome: 'Notebook Gamers Supreme',
+            marca: 'Dellta',
+            preco: 12500.50,
+            categoria: 'Informática',
+            Estoque: true
+        },
+        {
+            id: 3,
+            nome: 'Fone de Ouvido Sem Fio',
+            marca: 'Soni',
+            preco: 899.90,
+            categoria: 'Acessórios',
+            Estoque: false
+        },
+        {
+            id: 4,
+            nome: 'Smart TV 8K 75"',
+            marca: 'Elijê',
+            preco: 8400.00,
+            categoria: 'Eletrônicos',
+            Estoque: true
+        }
+    ];
 
-    const produto = {
-
-        id:1,
-        nome: 'Iphone 20 pro max',
-        marca: 'Semsunga',
-        preco: 67000.00,
-        categoria: 'Eletrônicos',
-        Estoque: true
-        
-    };
-    res.json(produto);
+    res.json(produtos);
 });
-app.listen(3000,() => {
 
+app.listen(3000, () => {
     console.log('server on door 3000');
 });
---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+async function carregarDados(){
+
+    const url = 'https://fantastic-waddle-gx794q49x946394vg-3000.app.github.dev/';
+
+    try{
+        const resposta = await fetch(url);
+
+        const produto = await resposta.json();
+
+        const areaProdutos = document.getElementById('lista-produtos');
+
+        areaProdutos.innerHTML = `
+     <div class="card">
+        <h2 class="nome-produto">${produto.nome}</h2>
+        <p class="categoria">Marca: ${produto.marca}</p>
+        <p class="preco">R$ ${produto.preco.toFixed(2)}</p>
+        <p><strong>Estoque:</strong> ${produto.emEstoque ? 'Disponível' : 'disponivel'}</p>
+      </div>
+    `;
+
+    } catch(erro) {
+        console.log('Erro ao carregar dados do produto:', erro);
+    }
+    
+
+}
+carregarDados();
